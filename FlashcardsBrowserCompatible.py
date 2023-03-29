f2df2c9272a36cc31c11e95ca642cc986a0a4d4b
Noah Potischman <noah.potischman@icloud.com>
2023-03-28 20:04:31 -0700

    Added all fcs and did final bug testing

--- FlashcardsBrowserCompatible.py
+++ FlashcardsBrowserCompatible.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-# flashcard class
+# Flashcard class
 class Flashcard:
     def __init__(self, prompt, answer):
         self.prompt = prompt
@@ -102,5 +102,44 @@ if __name__ == "__main__":
     flashcards = [
         Flashcard("What is the capital of France?", "Paris"),
         Flashcard("What is the capital of Italy?", "Rome"),
+        Flashcard("What is the capital of France?", "Paris"),
+        Flashcard("What is the capital of Italy?", "Rome"),
+        Flashcard("What is the capital of Germany?", "Berlin"),
+        Flashcard("What is the capital of the United States?", "Washington, D.C."),
+        Flashcard("What is the capital of Canada?", "Ottawa"),
+        Flashcard("What is the capital of the United Kingdom?", "London"),
+        Flashcard("What is the capital of Spain?", "Madrid"),
+        Flashcard("What is the capital of Australia?", "Canberra"),
+        Flashcard("What is the capital of Brazil?", "Brasília"),
+        Flashcard("What is the capital of Argentina?", "Buenos Aires"),
+        Flashcard("What is the capital of Russia?", "Moscow"),
+        Flashcard("What is the capital of China?", "Beijing"),
+        Flashcard("What is the capital of India?", "New Delhi"),
+        Flashcard("What is the capital of Japan?", "Tokyo"),
+        Flashcard("What is the capital of South Korea?", "Seoul"),
+        Flashcard("What is the capital of South Africa?", "Pretoria (executive), Bloemfontein (judicial), Cape Town (legislative)"),
+        Flashcard("What is the capital of Mexico?", "Mexico City"),
+        Flashcard("What is the capital of Colombia?", "Bogotá"),
+        Flashcard("What is the capital of Nigeria?", "Abuja"),
+        Flashcard("What is the capital of Egypt?", "Cairo"),
+        Flashcard("What is the capital of Turkey?", "Ankara"),
+        Flashcard("What is the capital of Iran?", "Tehran"),
+        Flashcard("What is the capital of Iraq?", "Baghdad"),
+        Flashcard("What is the capital of Saudi Arabia?", "Riyadh"),
+        Flashcard("What is the capital of United Arab Emirates?", "Abu Dhabi"),
+        Flashcard("What is the capital of Kuwait?", "Kuwait City"),
+        Flashcard("What is the capital of Qatar?", "Doha"),
+        Flashcard("What is the capital of Oman?", "Muscat"),
+        Flashcard("What is the capital of Yemen?", "Sana'a"),
+        Flashcard("What is the capital of Afghanistan?", "Kabul"),
+        Flashcard("What is the capital of Pakistan?", "Islamabad"),
+        Flashcard("What is the capital of Bangladesh?", "Dhaka"),
+        Flashcard("What is the capital of Thailand?", "Bangkok"),
+        Flashcard("What is the capital of Vietnam?", "Hanoi"),
+        Flashcard("What is the capital of Indonesia?", "Jakarta"),
+        Flashcard("What is the capital of Philippines?", "Manila"),
+        Flashcard("What is the capital of Malaysia?", "Kuala Lumpur"),
+        Flashcard("What is the capital of Singapore?", "Singapore"),
+        Flashcard("What is the capital of South Sudan?", "Juba")
     ]
     app.run(debug=True)
