fc778f0931d335544e94ceb4abc3e9d1b90f8a3e
Noah Potischman <noah.potischman@icloud.com>
2023-03-28 17:41:42 -0700

    HTML and Flash Changes

--- FlashcardsV2.py
+++ FlashcardsV2.py
@@ -1,6 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
 import random
 import json
 import os
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 # flashcard class
 class Flashcard:
@@ -54,89 +58,49 @@ def leitner_system(flashcards, user_data):
     1. Creates a list of flashcard instances
     2. Gets the users username and loads their data
     3. Runs the main loop """
-def main():
-    # Create a list of Flashcard instances
-    flashcards = [
-    Flashcard("What is the capital of France?", "Paris"),
-    Flashcard("What is the capital of Italy?", "Rome"),
-    Flashcard("What is the capital of Germany?", "Berlin"),
-    Flashcard("What is the capital of the United States?", "Washington, D.C."),
-    Flashcard("What is the capital of Canada?", "Ottawa"),
-    Flashcard("What is the capital of the United Kingdom?", "London"),
-    Flashcard("What is the capital of Spain?", "Madrid"),
-    Flashcard("What is the capital of Australia?", "Canberra"),
-    Flashcard("What is the capital of Brazil?", "Brasília"),
-    Flashcard("What is the capital of Argentina?", "Buenos Aires"),
-    Flashcard("What is the capital of Russia?", "Moscow"),
-    Flashcard("What is the capital of China?", "Beijing"),
-    Flashcard("What is the capital of India?", "New Delhi"),
-    Flashcard("What is the capital of Japan?", "Tokyo"),
-    Flashcard("What is the capital of South Korea?", "Seoul"),
-    Flashcard("What is the capital of South Africa?", "Pretoria (executive), Bloemfontein (judicial), Cape Town (legislative)"),
-    Flashcard("What is the capital of Mexico?", "Mexico City"),
-    Flashcard("What is the capital of Colombia?", "Bogotá"),
-    Flashcard("What is the capital of Nigeria?", "Abuja"),
-    Flashcard("What is the capital of Egypt?", "Cairo"),
-    Flashcard("What is the capital of Turkey?", "Ankara"),
-    Flashcard("What is the capital of Iran?", "Tehran"),
-    Flashcard("What is the capital of Iraq?", "Baghdad"),
-    Flashcard("What is the capital of Saudi Arabia?", "Riyadh"),
-    Flashcard("What is the capital of United Arab Emirates?", "Abu Dhabi"),
-    Flashcard("What is the capital of Kuwait?", "Kuwait City"),
-    Flashcard("What is the capital of Qatar?", "Doha"),
-    Flashcard("What is the capital of Oman?", "Muscat"),
-    Flashcard("What is the capital of Yemen?", "Sana'a"),
-    Flashcard("What is the capital of Afghanistan?", "Kabul"),
-    Flashcard("What is the capital of Pakistan?", "Islamabad"),
-    Flashcard("What is the capital of Bangladesh?", "Dhaka"),
-    Flashcard("What is the capital of Thailand?", "Bangkok"),
-    Flashcard("What is the capital of Vietnam?", "Hanoi"),
-    Flashcard("What is the capital of Indonesia?", "Jakarta"),
-    Flashcard("What is the capital of Philippines?", "Manila"),
-    Flashcard("What is the capital of Malaysia?", "Kuala Lumpur"),
-    Flashcard("What is the capital of Singapore?", "Singapore"),
-    Flashcard("What is the capital of South Sudan?", "Juba")
-    ]
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-    # get the users username and load their data
-    username = input("Enter your username: ")
+@app.route("/username", methods=["POST"])
+def get_username():
+    username = request.form["username"]
+    user_data = load_user_data(username)
+    return redirect(url_for("quiz", username=username))
+
+@app.route("/quiz/<username>", methods=["GET", "POST"])
+def quiz(username):
     user_data = load_user_data(username)
 
-    # main loop 
-    while True:
-        print("\nOptions:")
-        print("|--------------------------|")
-        print("| 1. Start quiz            |")
-        print("| 2. Show statistics       |")
-        print("| 3. Exit                  |")
-        print("|--------------------------|")
+    if request.method == "POST":
+        user_answer = request.form["user_answer"]
+        prompt = request.form["prompt"]
+        answer = request.form["answer"]
 
-
-        option = input("Enter option number: ")
-
-        if option == "1":
-            flashcards_to_show = leitner_system(flashcards, user_data)
-            random.shuffle(flashcards_to_show)
-            for card in flashcards_to_show:
-                user_answer = input(card.prompt + " ")
-                if user_answer.lower() == card.answer.lower():
-                    print("Correct!")
-                    user_data["correct"].append(card.prompt)
-                    card.correct_count += 1
-                else:
-                    print("Incorrect!")
-                    user_data["incorrect"].append(card.prompt)
-            save_user_data(username, user_data)
-
-        elif option == "2":
-            show_statistics(user_data)
-
-        elif option == "3":
-            print("Exiting...")
-            break
-
+        if user_answer.lower() == answer.lower():
+            flash("Correct!")
+            user_data["correct"].append(prompt)
         else:
-            print("Invalid option. Please try again.")
+            flash("Incorrect!")
+            user_data["incorrect"].append(prompt)
+
+        save_user_data(username, user_data)
+        return redirect(url_for("quiz", username=username))
+
+    flashcards_to_show = leitner_system(flashcards, user_data)
+    random.shuffle(flashcards_to_show)
+    card = flashcards_to_show[0]
+    return render_template("quiz.html", card=card, username=username)
+
+@app.route("/statistics/<username>")
+def show_statistics(username):
+    user_data = load_user_data(username)
+    return render_template("statistics.html", user_data=user_data)
 
 if __name__ == "__main__":
-    main()
+    # Create a list of Flashcard instances
+    flashcards = [
+        Flashcard("What is the capital of France?", "Paris"),
+        Flashcard("What is the capital of Italy?", "Rome"),
+    ]
+    app.run(debug=True)
